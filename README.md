4d7f3b204a876be1c442d436e8728330e5e6d87f
jaenwawe <nwawe.jae@gmail.com>
2022-01-05 20:36:03 -0600

    README.md

--- README.md
+++ README.md
@@ -1,24 +1,36 @@
-# README
+- TeamWork
+  A site to share algorithm challenges and solutions. Users can see challenges, list the corresponding solution information, and add both challenges and solutions.
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+Rails stores the User, Challenge, and Solution Models in a Redux store which avoids the need for a component higher in the component hierarchy to retrieve props that it will not use. Thunk, middleware for react, ensured that the Redux store was current with any incoming changes from client requests.
 
-Things you may want to cover:
+- User Stories
+  A user signups for the site or logins in with currrent information to create an account.
 
-* Ruby version
+  After logging in or signing up, the userr will see rows of challenges. They can immediately choose to view a challenges solution information or they can select the About Page or Logout.
 
-* System dependencies
+  When the user selects an individual challenge from the rows of challenges, they they can add another solution. Here solution can mean hint, clarifying question, or a solution to the question. That will be added to a list of solution rows that display on the page. Then
 
-* Configuration
+- Models  
+   User  
+   Solutions
+  Challenge
 
-* Database creation
+- Model Relationships
+  User 1 -> _ Solutions _ <- 1 Challenge
 
-* Database initialization
+- Configuration
 
-* How to run the test suite
+  ruby 2.7.4p191
+  Rails 6.1.4.4
+  React-Bootstrap
 
-* Services (job queues, cache servers, search engines, etc.)
+  In the parent folder, run bundle install
+  In the folder named Client, run npm start
 
-* Deployment instructions
+- Database creation/initialization
+  rails db:migrate db:seed
 
-* ...
+
+- Demo Video
+
+https://youtu.be/1mru3KUyTGw
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,13 @@ Solution.destroy_all
 
 puts "Seed Users"
 
+jae = User.create!(
+    username: "constant",
+    password: "123abc",
+    first_name: "Jae",
+    bio: "Laughs and eats at O(1) speed.  Programs in a for loop!"
+)
+
 jil = User.create!(
     username: "ologn",
     password: "123abc",
@@ -34,21 +41,89 @@ puts "Finished Seeding  Users"
 puts "Seed Challenges"
 popChallenge = Challenge.create!(
     title: "TwoArray",
-    question: "Recieve a non-empty array of sorted integers and return a seperate array ascending which squares the original element from the same index"
+    question: "Recieve a non-empty array of sorted integers and return a seperate array ascending which squares the original element from the same index."
 )
 
 vagueChallenge = Challenge.create!(
-    question: "Sort this array using merge sort then insertion sort and discusss the pros and cons"
+    title: "Merge and then Insertion Sort",
+    question: "Sort this array using merge sort then insertion sort and discusss the pros and cons."
 )
 
 
 mathChallenge = Challenge.create!(
-    title: "TwoSum",
-    question: "Accept an array of integers that will not be null.  Accept a target number.  Find two numbers in the first array that add up to the target integer."
+    title: "Height of Binary Tree",
+    question: "Complete the function height() which takes root node of the tree as input parameter and returns an integer denoting the height of the tree. If the tree is empty, return 0."
+)
+
+
+mathChallenge = Challenge.create!(
+    title: "Height of Binary Tree",
+    question: "Complete the function height() which takes root node of the tree as input parameter and returns an integer denoting the height of the tree. If the tree is empty, return 0."
 )
 puts "Finished Seeding Challenges"
 
+binary= Challenge.create!(
+    title: "Determine if Two Trees are Identical",
+    question: "Complete the function isIdentical() that takes two roots as parameters and returns true or false. The printing is done by the driver code."
 
+)
+
+solve= Challenge.create!(
+    title: "Symmetric Tree",
+    question: "Given a Binary Tree. Check whether it is Symmetric or not, i.e. whether the binary tree is a Mirror image of itself or not."
+)
+
+
+k= Challenge.create!(
+    title: "Kth largest element in BST",
+    question: "Given a Binary search tree. Your task is to complete the function which will return the Kth largest element without doing any modification in Binary Search Tree."
+)
+
+balance= Challenge.create!(
+    title: "Check for Balanced Tree",
+    question: "Given a binary tree, find if it is height balanced or not. A tree is height balanced if difference between heights of left and right subtrees is not more than one for all nodes of tree. "
+)
+
+
+
+array= Challenge.create!(
+    title: "Array to BST",
+    question: "Given a sorted array. Convert it into a Height balanced Binary Search Tree (BST). Find the preorder traversal of height balanced BST. If there exist many such balanced BST consider the tree whose preorder is lexicographically smallest.
+    Height balanced BST means a binary tree in which the depth of the left subtree and the right subtree of every node never differ by more than 1."
+)
+
+
+subTree = Challenge.create!(
+    title: "Check if subtree ",
+    question: "Given two binary trees with head reference as T and S having at most N nodes. The task is to check if S is present as subtree in T. A subtree of a tree T1 is a tree T2 consisting of a node in T1 and all of its descendants in T1."
+)
+
+puts "Finished Seeding Challenges"
+
+
+
+
+puts "Seed Solutions"
+Solution.create!(
+    user_id: jae.id ,
+    challenge_id: subTree.id,
+    hints: "There should be a O(n) notation solution when an array  is already sorted.",
+    questions: "Could the elements in the array be negative?"
+)
+
+Solution.create!(
+    user_id: jil.id ,
+    challenge_id: solve.id,
+    explaination: "Sort the array.  Add the left and the right elements.  These are actually pointers that will move while iterating.  See if you need a higher or lower value to  hit the target.  If your need to reduce the prior total remove the highest value, and vice versa",
+
+)
+
+
+Solution.create!(
+    user_id: jae.id,
+    challenge_id: mathChallenge.id,
+    questions: "Is there a way there a solution that can add more than two numbers to equal a tota?"
+)
 
 puts "Seed Solutions"
 Solution.create!(
@@ -60,13 +135,10 @@ Solution.create!(
 
 Solution.create!(
     user_id: jil.id ,
-    challenge_id: mathChallenge.id,
+    challenge_id: balance.id,
     explaination: "Sort the array.  Add the left and the right elements.  These are actually pointers that will move while iterating.  See if you need a higher or lower value to  hit the target.  If your need to reduce the prior total remove the highest value, and vice versa",
 
 )
-
-
-
 
 
 Solution.create!(
@@ -85,7 +157,7 @@ Solution.create!(
 
 Solution.create!(
     user_id: pop.id,
-    challenge_id: popChallenge.id,
+    challenge_id: k.id,
     explaination: "JavaScript has a Math.pow() function to add the absolute value to a create! arrray.  As you add each element use insertion sort to create another",
     questions: "If the interviewers will not accept a JavaSript built in method then, will array[i] * array[i] be old fashioned enough for them :) ? " ,
 
